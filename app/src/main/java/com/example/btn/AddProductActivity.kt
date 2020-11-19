b61c1f794b88e4a8a55fbe0e8a4d544abf3954cb
F-Fikus <fikus.f.60@mail.ru>
2020-11-19 18:12:21 +0700

    init

---
+++ app/src/main/java/com/example/btn/AddProductActivity.kt
@@ -0,0 +1,13 @@
+package com.example.btn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddProductActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.asss)
+    }
+
+
+}
